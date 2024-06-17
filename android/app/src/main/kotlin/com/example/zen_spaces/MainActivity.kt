c520ca84b61770605d825df3548afc6efb5ffaed
Leszek W. Król <contact@leszekkrol.com>
2024-06-17 10:40:13 +0200

    ⚒️ Prepare app manifest & styles

--- android/app/src/main/kotlin/com/example/zen_spaces/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/zen_spaces/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.zen_spaces
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode.transparent
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", transparent.toString())
+        super.onCreate(savedInstanceState)
+    }
+}
